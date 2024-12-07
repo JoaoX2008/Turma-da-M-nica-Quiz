6600988dfed405d510f079ca7f2e19b59371a577
JoaoX2008 <0000111865271xsp@al.educacao.sp.gov.br>
2024-12-07 01:02:17 -0300

    Add files via upload

---
+++ João/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Turma da Mônica',
+    'Quem é o melhor amigo da Mônica?',
+    'O melhor amigo da Mônica é o Cebolinha.'
+);
+
+criaCartao(
+    'Turma da Mônica',
+    'Qual é o nome do bairro onde moram a Mônica e seus amigos?',
+    'A Turma da Mônica mora no bairro do Limoeiro.'
+);
+
+criaCartao(
+    'Turma da Mônica',
+    'Quem é o cachorro da Mônica?',
+    'O cachorro da Mônica se chama Bidu.'
+);
+
+criaCartao(
+    'Turma da Mônica',
+    'Qual é o nome do personagem que vive tentando pegar o coelhinho da Mônica?',
+    'O personagem que tenta pegar o coelhinho da Mônica é o Sansão.'
+);
